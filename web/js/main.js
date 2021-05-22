72663ea89a9849777a5cfc5d6b7e87c90cee83bf
daherrerac <74057388+daherrerac@users.noreply.github.com>
2021-05-22 12:01:44 -0500

    Actualización
    Cambio de librerias

---
+++ web/js/main.js
@@ -0,0 +1,54 @@
+
+
+$( "#trigger-qb" ).click(function(event) {
+    event.preventDefault();
+    $( "#chart" ).toggle( function() {
+      // Animation complete.
+    });
+    $( "#chartalt" ).toggle(  function() {
+        // Animation complete.
+    });
+    $( "#chart-2" ).toggle(  function() {
+        // Animation complete.
+    });
+    $( "#chartalt2" ).toggle(  function() {
+        // Animation complete.
+    });
+});
+$( "#trigger-qc" ).click(function(event) {
+    event.preventDefault();
+    $( "#chart" ).toggle(  function() {
+      // Animation complete.
+    });
+    $( "#chartalt" ).toggle(  function() {
+        // Animation complete.
+    });
+    $( "#chart-2" ).toggle(  function() {
+        // Animation complete.
+    });
+    $( "#chartalt2" ).toggle(  function() {
+        // Animation complete.
+    });
+});
+
+$( "#reg1" ).click(function(event) {
+    event.preventDefault();
+    $( "#chartb" ).toggle(  function() {
+      // Animation complete.
+    });
+    $( "#chartbalt" ).toggle(  function() {
+        // Animation complete.
+    });
+    
+});
+
+$( "#reg2" ).click(function(event) {
+    event.preventDefault();
+    $( "#chartc" ).toggle(  function() {
+      // Animation complete.
+    });
+    $( "#chartcalt" ).toggle(  function() {
+        // Animation complete.
+    });
+    
+});
